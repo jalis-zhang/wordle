8ebe525cf0ca4bd549c915ba2014db86bd91f104
zhangjialiang12 <zhangjialiang12@shuidi-inc.com>
2023-09-01 13:34:17 +0800

    wordle 添加主逻辑

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use std::io;
+use std::process::exit;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut rows = Vec::new();
+    rows.push(String::from("sdfasdf"));
+    rows.push(String::from("slkdwopeir"));
+
+    println_vec(&rows);
+
+
+    let row = rows.get(0).unwrap();
+    println!("{row}");
+
+
+    let compare_result = compare("sdf", "sdf");
+    println!("{:?}", compare_result);
+
+
+    let mut round = 0;
+
+    loop {
+
+        if (round == 6) {
+            println!("Do you want to continue the game ? [Y/N]");
+            let mut is_continue = String::new();
+            io::stdin().read_line(&mut is_continue).unwrap();
+
+            if is_continue.eq("Y") {
+                round = 0;
+            } else if is_continue.eq("N") {
+                exit(0);
+            } else {
+                println!("unknown character, please input again. [Y/N]");
+            }
+        } else {
+            let mut line = String::new();
+            io::stdin().read_line(&mut line).unwrap();
+            round += 1;
+        }
+    }
+
+}
+
+
+fn println_vec(rows: &Vec<String>) {
+    for str in rows {
+        println!("{str}")
+    }
+}
+
+
+fn compare(str1: &str, str2: &str) -> Vec<bool> {
+
+    let bytes1 = str1.as_bytes();
+    let bytes2 = str2.as_bytes();
+
+    let mut result = Vec::new();
+
+    for idx in 0..bytes1.len() {
+        if bytes1[idx] == bytes2[idx] {
+            result.push(true);
+        } else {
+            result.push(false);
+        }
+    }
+    result
 }
